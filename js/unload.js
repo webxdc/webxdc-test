04e2688de239ecadcf8cf14ba9ba8580c298ed22
B. Petersen <r10s@b44t.com>
2022-05-17 23:52:46 +0200

    also check onbeforeunload

--- js/unload.js
+++ js/unload.js
@@ -1,12 +1,20 @@
 
 window.addEventListener("load", () => {
     var storage = window.localStorage;
-    var count = parseInt(storage.getItem("onunload-test") || 0);
+    var onunloadCount = parseInt(storage.getItem("onunload-test") || 0);
+    var onbeforeunloadCount = parseInt(storage.getItem("onbeforeunload-test") || 0);
 
     var output = document.getElementById("unload-output");
     output.innerHTML = "<h1>unload</h1>";
-    output.innerHTML += "unload event called " + count + " times";
+    output.innerHTML += "unload event triggered " + onunloadCount + " times<br>";
+    output.innerHTML += "beforeunload event triggered " + onbeforeunloadCount + " times";
     output.innerHTML += "<hr>";
+});
+
+window.addEventListener("beforeunload", () => {
+    var storage = window.localStorage;
+    var count = parseInt(storage.getItem("onbeforeunload-test") || 0) + 1;
+    storage.setItem("onbeforeunload-test", count);
 });
 
 window.addEventListener("unload", () => {
