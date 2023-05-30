2f905d669ea8477b0d737536352859194c87135e
adbenitez <asieldbenitez@gmail.com>
2023-05-30 14:31:16 +0200

    improve the file selector input, display image preview

--- js/uploads.js
+++ js/uploads.js
@@ -1,6 +1,23 @@
+function setInpPrev(event) {
+    var img = document.getElementById('inpImgPreview');
+    if (event.target.files.length == 0) {
+        img.src = "#";
+    } else {
+        var reader = new FileReader();
+        reader.onload = function() {
+            img.src = reader.result;
+        };
+        reader.readAsDataURL(event.target.files[0]);
+    }
+}
+
 window.addEventListener("load", () => {
     document.getElementById("uploads-output").append(
         createHeader("Uploads"),
-        h("div", {class: "container"}, h('input', {type: "file"}))
+        h("div", {class: "container"},
+          h('input', {id: "imgInp", type: "file", accept: "image/*", onchange: "setInpPrev(event)"}),
+          h("br"), h("br"),
+          h("img", {id: "inpImgPreview", style: "max-height: 200px; max-width: 90%; width: auto; display:block", alt: "Image preview will appear here"}),
+         ),
     );
 });
