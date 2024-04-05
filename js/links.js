0b62805f6a8108c574f33e6e837f2078db46fb29
adbenitez <asieldbenitez@gmail.com>
2024-04-05 19:12:19 -0400

    fix property name

--- js/links.js
+++ js/links.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
     let ul = h("ul");
     ul.append(
-        h("li", {}, h("a", {href: "https://delta.chat"}, "Normal link: https://delta.chat (should work if sent in Saved Messages chat due to internet_access property set in manifest)")),
+        h("li", {}, h("a", {href: "https://delta.chat"}, "Normal link: https://delta.chat (should work if sent in Saved Messages chat due to request_internet_access property set in manifest)")),
         h("li", {}, h("a", {href: "mailto:delta@example.org?body=test+message"}, "Mailto link")),
         h("li", {}, h("a", {href: "OPENPGP4FPR:571E6FDC22C1605512A1B0C8F7AC9331B82AFB5B#a=delta%40example.org&n=TestContact&i=pHMb3fRw-JV&s=VcWU-pQSEeB"}, "QR verification link")),
         h("li", {}, h("a", {href: "cabal://cabal.chat"}, "Custom scheme link")),
