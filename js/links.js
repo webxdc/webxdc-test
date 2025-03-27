757b1fe5b8ee65f15c72873778c3552a52049740
Simon Laux <mobile.info@simonlaux.de>
2025-03-28 00:22:07 +0100

    split link card into multiple cards for the different tabs

--- js/links.js
+++ js/links.js
@@ -9,30 +9,7 @@ window.addEventListener("load", () => {
         h("li", {}, h("a", {href: "chrome://crash"}, "chrome://crash")),
     );
 
-    let btn1 = h("button", {}, "info with deep-link to internal page, no notify");
-    btn1.onclick = () => {
-        window.webxdc.sendUpdate({ payload: "", info: "open page.html", href: "page.html" }, "");
-    };
-    ul.append(h("li", {}, btn1));
-
-    let btn2 = h("button", {}, "info and notify all with deep-link to internal page");
-    btn2.onclick = () => {
-        window.webxdc.sendUpdate({ payload: "", info: "open page.html", href: "page.html", notify: {"*": "notify page.html"} }, "");
-    };
-    ul.append(h("li", {}, btn2));
-
-    let btn3 = h("button", {}, "info and notify all with deep-link to #links-output section");
-    btn3.onclick = () => {
-        window.webxdc.sendUpdate({ payload: "", info: "open #links-output", href: "index.html#links-output", notify: {"*": "notify #links-output"} }, "");
-    };
-    ul.append(h("li", {}, btn3));
-
-    const ifrmExplain = h("p", {}, "iframe: should be blocked and not load https://delta.chat from Internet:");
-    const ifrm = h("iframe", {src: "https://delta.chat"});
-    ifrm.style.width = "100%";
-    ifrm.style.height = "auto";
-
     document.getElementById("links-output").append(
-        createHeader("Links"), h("div", {class: "container"}, ul, ifrmExplain, ifrm)
+        createHeader("Links"), h("div", {class: "container"}, ul)
     );
 });
