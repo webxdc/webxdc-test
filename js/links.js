0da54cdd935ce8992852faf5a3f93322d9b42920
adbenitez <asieldbenitez@gmail.com>
2025-11-23 18:30:01 +0100

    add target=_blank for https link

--- js/links.js
+++ js/links.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
     let ul = h("ul");
     ul.append(
-        h("li", {}, h("a", {href: "https://delta.chat"}, "Normal link: https://delta.chat (should be blocked)")),
+        h("li", {}, h("a", {target:"_blank", href: "https://delta.chat"}, "Normal link: https://delta.chat (should ask to open)")),
         h("li", {}, h("a", {href: "mailto:delta@example.org?body=test+message"}, "Mailto link")),
         h("li", {}, h("a", {href: "OPENPGP4FPR:571E6FDC22C1605512A1B0C8F7AC9331B82AFB5B#a=delta%40example.org&n=TestContact&i=pHMb3fRw-JV&s=VcWU-pQSEeB"}, "old openpgp4fpr: QR verification link")),
         h("li", {}, h("a", {target:"_blank", href: "https://i.delta.chat/#9AF055DB87EC48A1C009B6CA55E3712A6F7D346F&a=botsindex%40nine.testrun.org&n=Public%20Bots&i=QpBSronexvP&s=nAfQ0q_JomN"}, "New i.delta.chat invite link")),
