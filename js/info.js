0b9c3c467747b063989791be63f42ed7b1a510ac
adbenitez <asieldbenitez@gmail.com>
2026-04-18 22:11:09 +0200

    add new webxdc.isAppSender and webxdc.isBroadcast APIs

--- js/info.js
+++ js/info.js
@@ -5,6 +5,9 @@ window.addEventListener("load", () => {
     container.append(h("div", {}, "webxdc.selfName: ", h("span", {style: "background:#DDD;word-wrap: break-word;"}, webxdc.selfName)))
     container.append(h("div", {}, "webxdc.selfAddr: ", h("span", {style: "background:#DDD;word-wrap: break-word;"}, webxdc.selfAddr)))
 
+    container.append(h("div", {}, "webxdc.isAppSender: ", webxdc.isAppSender));
+    container.append(h("div", {}, "webxdc.isBroadcast: ", webxdc.isBroadcast));
+
     container.append(h("div", {}, "webxdc.sendUpdateInterval: " + webxdc.sendUpdateInterval))
     container.append(h("div", {}, "webxdc.sendUpdateMaxSize: " + webxdc.sendUpdateMaxSize))
 
